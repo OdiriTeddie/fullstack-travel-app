e006c5cbafd29bb87b5cc0f803af6a05242129cf
Odiri Teddie <odiriteddie.adamu@gmail.com>
2024-03-20 10:49:39 -0700

    fullstack pwa travelweb app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -33,12 +33,6 @@ export default defineConfig({
             type: "image/png",
             purpose: "apple touch icon",
           },
-          {
-            src: "/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
         ],
         theme_color: "#181818",
         background_color: "#e0cc3b",
